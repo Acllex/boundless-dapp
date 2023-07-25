f53d129ee26ff07ae5cc0b6cfaba4065a383343e
Acllex <w986169823@gmail.com>
2023-07-25 23:29:16 +0800

    'creat(fe):初始化前端项目'

---
+++ boundless-fe/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="flex justify-center">
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
