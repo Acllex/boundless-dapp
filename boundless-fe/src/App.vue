c903affd172184492875a48da7dc31f2a3800bc7
Acllex <w986169823@gmail.com>
2023-07-26 22:19:51 +0800

    添加表单页面

--- boundless-fe/src/App.vue
+++ boundless-fe/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {RouterLink, RouterView} from 'vue-router'
-import {ref} from 'vue'
+import { RouterView } from 'vue-router'
+import { ref } from 'vue'
 
 const activeIndex = ref('/')
 const handleSelect = (key: string, keyPath: string[]) => {
@@ -9,33 +9,32 @@ const handleSelect = (key: string, keyPath: string[]) => {
 </script>
 
 <template>
-  <div class=" max-w-7xl mx-auto space-y-8 sm:px-6 lg:px-8">
-    <el-menu
-        :default-active="activeIndex"
-        :router="true"
-        mode="horizontal"
-        :ellipsis="false"
-        @select="handleSelect"
-    >
+  <div class="bg-gray-50 min-h-screen">
+    <div class="min-w-full bg-white sm:px-6 lg:px-8">
+      <div class="max-w-7xl mx-auto">
+        <el-menu
+          :default-active="activeIndex"
+          :router="true"
+          mode="horizontal"
+          :ellipsis="false"
+          @select="handleSelect"
+        >
+          <el-menu-item index="/"> Home </el-menu-item>
+          <el-menu-item index="/about"> 创造 </el-menu-item>
+          <div class="flex-1"></div>
+          <el-menu-item index="2">
+            <div class="h-full box-border">
+              <el-avatar class="mb-1.5" src="" />
+            </div>
+          </el-menu-item>
+        </el-menu>
+      </div>
+    </div>
 
-      <el-menu-item index="/">
-        Home
-      </el-menu-item>
-      <el-menu-item index="/about">
-        About
-      </el-menu-item>
-      <div class=" flex-1"></div>
-      <el-menu-item index="2">
-        <div class="h-full box-border">
-          <el-avatar class=" mb-1.5" src=""/>
-        </div>
-      </el-menu-item>
-    </el-menu>
-  </div>
-
-  <div class=" py-16 bg-gray-50 overflow-hidden min-h-screen">
-    <div class="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
-      <RouterView/>
+    <div class="py-8 min-w-full overflow-hidden">
+      <div class="max-w-7xl bg-white mx-auto px-4 py-8 sm:px-6 lg:px-8">
+        <RouterView />
+      </div>
     </div>
   </div>
 </template>
